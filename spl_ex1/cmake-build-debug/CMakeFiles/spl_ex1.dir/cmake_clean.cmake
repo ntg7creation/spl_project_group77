81378e91b86a00f591a1ad45ac6d67419c0db83c
natai <ntg7creation@gmail.com>
2018-11-06 12:35:14 +0200

    load
    load file objects

---
+++ spl_ex1/cmake-build-debug/CMakeFiles/spl_ex1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spl_ex1.dir/main.cpp.obj"
+  "spl_ex1.pdb"
+  "spl_ex1.exe"
+  "spl_ex1.exe.manifest"
+  "spl_ex1.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spl_ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
