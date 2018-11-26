7bea84485a0efb335b67b336ad7a29d47567cca9
natai <ntg7creation@gmail.com>
2018-11-26 17:03:29 +0200

    test

--- main/java/bgu/spl/mics/application/BookStoreRunner.java
+++ main/java/bgu/spl/mics/application/BookStoreRunner.java
@@ -5,6 +5,6 @@ package bgu.spl.mics.application;
  * In the end, you should output serialized objects.
  */
 public class BookStoreRunner {
-   // public static void main(String[] args) {
-    //}
+    public static void main(String[] args) {
+    }
 }
