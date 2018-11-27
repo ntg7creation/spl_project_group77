14e50a8a5f990a9e569ae83a7fbed13cbc1b3813
natai <ntg7creation@gmail.com>
2018-11-27 15:12:46 +0200

    future test

---
+++ main/java/Future_Test.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Future_Test {
+
+	@Test
+	public void testFuture() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResolve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsDone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLongTimeUnit() {
+		fail("Not yet implemented");
+	}
+
+}
