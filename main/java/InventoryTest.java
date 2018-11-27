661db748b2d43c057cfd2191ca1e66c09b0c6630
Arthur <arthur793@gmail.com>
2018-11-27 14:07:09 +0200

    gsdg

---
+++ main/java/InventoryTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InventoryTest {
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTake() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckAvailabiltyAndGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintInventoryToFile() {
+		fail("Not yet implemented");
+	}
+
+}
