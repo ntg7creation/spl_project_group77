e00b63d2e5a5af96a37dd57c485a7472d1ed5932
Arthur <arthur793@gmail.com>
2018-12-16 18:44:23 +0200

    dss

--- main/java/bgu/spl/mics/application/messages/DeliveryEvent.java
+++ main/java/bgu/spl/mics/application/messages/DeliveryEvent.java
@@ -4,7 +4,7 @@ import bgu.spl.mics.Event;
 import bgu.spl.mics.application.passiveObjects.BookInventoryInfo;
 import bgu.spl.mics.application.passiveObjects.Customer;
 
-public class DeliveryEvent implements Event<Object> {
+public class DeliveryEvent implements Event<Boolean> {
 
 	private BookInventoryInfo book;
 	private Customer c;
