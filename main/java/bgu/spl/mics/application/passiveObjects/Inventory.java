e76f640b7c5b25d8d38ba41d2edfa4322a4402f1
Arthur <arthur793@gmail.com>
2018-12-16 21:07:54 +0200

    cxv

--- main/java/bgu/spl/mics/application/passiveObjects/Inventory.java
+++ main/java/bgu/spl/mics/application/passiveObjects/Inventory.java
@@ -1,6 +1,10 @@
 package bgu.spl.mics.application.passiveObjects;
-import java.util.concurrent.ConcurrentHashMap;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
 
 /**
  * Passive data-object representing the store inventory. It holds a collection
@@ -13,13 +17,14 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class Inventory {
 
-	private ConcurrentHashMap <String,Integer> amount;
-	private ConcurrentHashMap <String,Integer> price;
-
+	private ConcurrentHashMap<String, Integer> amount;
+	private ConcurrentHashMap<String, Integer> price;
+	private ConcurrentLinkedQueue<String> books;
 
 	private Inventory() {
-	amount = new ConcurrentHashMap <>();
-	price = new ConcurrentHashMap <>();
+		amount = new ConcurrentHashMap<>();
+		price = new ConcurrentHashMap<>();
+		books = new ConcurrentLinkedQueue<>();
 
 	}
 
@@ -40,17 +45,16 @@ public class Inventory {
 	 * store inventory.
 	 * <p>
 	 * 
-	 * @param inventory
-	 *            Data structure containing all data necessary for initialization of
-	 *            the inventory.
+	 * @param inventory Data structure containing all data necessary for
+	 *                  initialization of the inventory.
 	 */
 	public void load(BookInventoryInfo[] inventory) {
-	//	ConcurrentHashMap <String,Integer> amount = new ConcurrentHashMap <>();
-	//	ConcurrentHashMap <String,Integer> price = new ConcurrentHashMap <>();
+		// ConcurrentHashMap <String,Integer> amount = new ConcurrentHashMap <>();
+		// ConcurrentHashMap <String,Integer> price = new ConcurrentHashMap <>();
 
-		for(int i = 0 ; i < inventory.length ; i++){
-			price.put(inventory[i].getBookTitle() , inventory[i].getPrice());
-			amount.put(inventory[i].getBookTitle() , inventory[i].getAmountInInventory());
+		for (int i = 0; i < inventory.length; i++) {
+			price.put(inventory[i].getBookTitle(), inventory[i].getPrice());
+			amount.put(inventory[i].getBookTitle(), inventory[i].getAmountInInventory());
 		}
 
 	}
@@ -59,8 +63,7 @@ public class Inventory {
 	 * Attempts to take one book from the store.
 	 * <p>
 	 * 
-	 * @param book
-	 *            Name of the book to take from the store
+	 * @param book Name of the book to take from the store
 	 * @return an {@link Enum} with options NOT_IN_STOCK and SUCCESSFULLY_TAKEN. The
 	 *         first should not change the state of the inventory while the second
 	 *         should reduce by one the number of books of the desired type.
@@ -72,52 +75,51 @@ public class Inventory {
 				if (count > 0) {
 					return OrderResult.Success;
 				}
-				//count= count- 1;
+				// count= count- 1;
 			}
 		}
 		return OrderResult.OutOfStock;
-		
-		//		OrderResult result = OrderResult.DontHaveBook;
-		//		for (BookInventoryInfo bookInventoryInfo : inventory) {
-		//			if (bookInventoryInfo.getBookTitle().equals(book)) {
-		//				result = OrderResult.OutOfStock;
-		//				if (bookInventoryInfo.removeBooks(1)) {
-		//					return OrderResult.Success;
-		//				}
-		//			}
-		//		}
-		//		return result;
+
+		// OrderResult result = OrderResult.DontHaveBook;
+		// for (BookInventoryInfo bookInventoryInfo : inventory) {
+		// if (bookInventoryInfo.getBookTitle().equals(book)) {
+		// result = OrderResult.OutOfStock;
+		// if (bookInventoryInfo.removeBooks(1)) {
+		// return OrderResult.Success;
+		// }
+		// }
+		// }
+		// return result;
 	}
 
 	/**
 	 * Checks if a certain book is available in the inventory.
 	 * <p>
 	 * 
-	 * @param book
-	 *            Name of the book.
+	 * @param book Name of the book.
 	 * @return the price of the book if it is available, -1 otherwise.
 	 */
 	public int checkAvailabiltyAndGetPrice(String book) {
-		
-        Integer count = amount.get(book);
-        if (count != null) {
-            //synchronized (book) { not needed
-                if (count> 0)
-                    return price.get(book);
-           // }
-        }
-        return -1;
+
+		Integer count = amount.get(book);
+		if (count != null) {
+			// synchronized (book) { not needed
+			if (count > 0)
+				return price.get(book);
+			// }
+		}
+		return -1;
 	}
-	
-//		for (BookInventoryInfo bookInventoryInfo : inventory) {
-//			if (bookInventoryInfo.getBookTitle().equals(book)) {
-//				if (bookInventoryInfo.getAmountInInventory() > 0) {
-//					return bookInventoryInfo.getPrice();
-//				}
-//			}
-//		}
-//		return -1;
-//	}
+
+	// for (BookInventoryInfo bookInventoryInfo : inventory) {
+	// if (bookInventoryInfo.getBookTitle().equals(book)) {
+	// if (bookInventoryInfo.getAmountInInventory() > 0) {
+	// return bookInventoryInfo.getPrice();
+	// }
+	// }
+	// }
+	// return -1;
+	// }
 
 	/**
 	 * 
@@ -130,73 +132,20 @@ public class Inventory {
 	 * output.
 	 */
 	public void printInventoryToFile(String filename) {
-		
-//		ConcurrentHashMap <String,Integer> BooksConcurrentHashMap = new ConcurrentHashMap <>();
-//		for(BookInventoryInfo bookInfo: books){
-//			BooksConcurrentHashMap.put(bookInfo.getBookTitle(), bookInfo.getAmountInInventory());
-//			try {
-//				FileOutputStream fileOut = new FileOutputStream(filename);
-//				ObjectOutputStream out = new ObjectOutputStream(fileOut);
-//				out.writeObject(BooksConcurrentHashMap);
-//				out.close();
-//				fileOut.close();
-//				System.out.printf("Serialized data is saved in " + filename);
-//			} catch (IOException i) {
-//				i.printStackTrace();
-//		
-//			
-//
-//		}
-//			booksHashMap.put(bookInfo.getBookTitle(), bookInfo.getAmountInInventory());			String name = book.getBookTitle();
-//			Integer amount = book.getAmountInInventory();
-//			inventoryToFile.put(name,amount);
-//			
-////            FileOutputStream fos =
-////                    new FileOutputStream("hashmap.ser");
-////                 ObjectOutputStream oos = new ObjectOutputStream(fos);
-////                 oos.writeObject(hmap);
-////                 oos.close();
-////                 fos.close();
-////		}
-////		filePrinter.printToFile(inventoryToFile,filename);
-////		
-////		BookMap.putAll(AmountMap);
-////		try
-////		{
-////			//Saving of object in a file
-////			FileOutputStream file = new FileOutputStream(filename);
-////			ObjectOutputStream out = new ObjectOutputStream(file);
-////
-////			// Method for serialization of object
-////			out.writeObject(BookMap);
-////			out.close();
-////			file.close();
-////		}
-////		catch(IOException ex)
-//			
-//			
-//			
-//			HashMap<String, Integer> booksHashMap = new HashMap<>();
-//			for(BookInventoryInfo bookInfo: books){
-//				booksHashMap.put(bookInfo.getBookTitle(), bookInfo.getAmountInInventory());
-//			}
-//		
-//		
-//		
-//		{
-//			System.out.println("IOException is caught");
-//
-//		
-//		
-//		
-////		synchronized (Theinventory){
-////			ConcurrentHashMap<String, Integer> booksHashMap = new ConcurrentHashMap<>();
-////			for(BookInventoryInfo bookInfo: inventory){
-////				booksHashMap.put(bookInfo.getBookTitle(), bookInfo.getAmountInInventory());
-////				// need to complete
-////			}
+
+		try {
+				FileOutputStream fileOut = new FileOutputStream(filename);
+				ObjectOutputStream out = new ObjectOutputStream(fileOut);
+				out.writeObject(amount);
+				out.close();
+				fileOut.close();
+				System.out.println("Data is saved in " + filename);
+			} catch (IOException i) {
+				i.printStackTrace();
+
+
+
+
+			}
 		}
 }
-	
-	
-
