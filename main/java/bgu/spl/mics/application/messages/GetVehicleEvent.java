bbdd9302751d697480977e7b2c9bb830643d4756
natai <ntg7creation@gmail.com>
2018-12-19 13:08:19 +0200

    s

--- main/java/bgu/spl/mics/application/messages/GetVehicleEvent.java
+++
@@ -1,8 +0,0 @@
-package bgu.spl.mics.application.messages;
-
-import bgu.spl.mics.Event;
-import bgu.spl.mics.Future;
-import bgu.spl.mics.application.passiveObjects.DeliveryVehicle;
-
-public class GetVehicleEvent implements Event<Future<DeliveryVehicle>> {
-	   	}
